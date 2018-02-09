f6ca05863a71f9369ff1cc54c109ace1b207d169
zanbus5683 <36283941+zanbus5683@users.noreply.github.com>
2018-02-08 17:10:59 -0700

    Create StudentDerivationFromPerson.java

---
+++ StudentDerivationFromPerson.java
@@ -0,0 +1,15 @@
+package javaapplication1;
+
+/**
+ *
+ * @author josep
+ */
+// ===== Code from file StudentDerivationFromPerson.java =====
+public class StudentDerivationFromPerson {
+   public static void main (String [] args) {
+      StudentData courseStudent = new StudentData();
+
+      /* Your solution goes here  */
+    
+   }
+}
